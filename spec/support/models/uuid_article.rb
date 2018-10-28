739a6870ebb1bb348d4dfa4cbe71aa2b95a2a0eb
Sebastian Skałacki <skalee@gmail.com>
2018-10-28 20:53:39 +0100

    Use ActiveUUID::ColumnType in test models
    See: https://api.rubyonrails.org/classes/ActiveRecord/Attributes/ClassMethods.html#method-i-attribute

--- spec/support/models/uuid_article.rb
+++ spec/support/models/uuid_article.rb
@@ -1,3 +1,6 @@
 class UuidArticle < ActiveRecord::Base
   include ActiveUUID::UUID
+
+  attribute :id, ActiveUUID::ColumnType.new
+  attribute :another_uuid, ActiveUUID::ColumnType.new
 end
--- spec/support/models/uuid_article_with_natural_key.rb
+++ spec/support/models/uuid_article_with_natural_key.rb
@@ -1,5 +1,9 @@
 class UuidArticleWithNaturalKey < ActiveRecord::Base
   include ActiveUUID::UUID
   self.table_name = "uuid_articles"
+
+  attribute :id, ActiveUUID::ColumnType.new
+  attribute :another_uuid, ActiveUUID::ColumnType.new
+
   natural_key :title
 end
